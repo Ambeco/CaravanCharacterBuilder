2dae07fa95cec35639442c4099ee7ed37b19ddf6
Ambeco <mooing_psycho_duck@yahoo.com>
2018-04-30 22:39:08 -0700

    Character creation points and xp will have to be explained to the user

--- CaravanBuilder/js/types/Currency.ts
+++ CaravanBuilder/js/types/Currency.ts
@@ -4,18 +4,24 @@
  * Cost=5 Xp.  Currency=Xp.
  * Requirement=3 Divine.  Currency=Divine.
  **/
-export let namedCurrencies: Map <string, Currency> = new Map<string, Currency>();
+export let namedCurrencies: Map<string, Currency> = new Map<string, Currency>();
+export let importantCurrencies: Set<Currency> = new Set<Currency>();
 export class Currency {
     private readonly name: string;
     private readonly initialCount: number;
+    private readonly justifyToUser: boolean;
     private readonly conversions: Map<Currency, number>; //1.5 means 1 of this is 1.5 of that
 
-    constructor(name: string, initialCount: number) {
+    constructor(name: string, initialCount: number, justifyToUser: boolean) {
         this.name = name;
         this.initialCount = initialCount;
+        this.justifyToUser = justifyToUser;
         this.conversions = new Map<Currency, number>();
         if (namedCurrencies.get(name) != null) throw "two currencies with name " + name;
         namedCurrencies.set(name, this);
+        if (justifyToUser) {
+            importantCurrencies.add(this);
+        }
     }
     getName(): string { return this.name; }
     getInitialCount(): number { return this.initialCount; }
